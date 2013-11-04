a8110e4c86a009d415149c69b5163f859edcd7b4
d-salazar <d.salazar.sepulveda@gmail.com>
2013-11-04 18:30:13 -0300

    Clase 5

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,39 @@
 class PostsController < ApplicationController
 	def new
+		@post = Post.new()
 	end
-	#def create
-	#	render text: params[:post].inspect
-	#end
-	def create
-		@post = Post.new(post_params)
 
-		@post.save
-		redirect_to @post
+	def create
+		@post = Post.new(params[:post].permit(:title,:text))
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(params[:post].permit(:title, :text))
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+	end
+
 	def show
 		@post = Post.find(params[:id])
 	end
@@ -17,6 +41,8 @@ class PostsController < ApplicationController
 	def index
 		@posts = Post.all
 	end
+
+
 
 	private 
 		def post_params
